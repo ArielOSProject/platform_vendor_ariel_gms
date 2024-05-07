ceab0d90ee23f319d894e3858b59b3f6adb620e5
Sava Mikalacki <sava@codekrafters.com>
2024-05-07 14:49:31 +0200

    Update apk names, conditional optional libs support

--- GoogleContactsSyncAdapter/Android.mk
+++ GoogleContactsSyncAdapter/Android.mk
@@ -7,9 +7,11 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
+#com.google.android.syncadapters.contacts_11-6684105-30_minAPI30(nodpi)_apkmirror.com.apk
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := GoogleContactsSyncAdapter
-LOCAL_SRC_FILES := com.google.android.syncadapters.contacts_11-6684105-30_minAPI30(nodpi)_apkmirror.com.apk
+LOCAL_SRC_FILES := com.google.android.syncadapters.contacts.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
