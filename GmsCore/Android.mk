e4316727c9f29f685788e0b7c214418db6c78f85
Sava Mikalacki <mikalackis@gmail.com>
2025-03-25 11:26:59 +0100

    Include microg.xml
    Change-Id: Ie297dd2ebd43eeab1b1f20b14eb5b6e01552897f

--- GmsCore/Android.mk
+++ GmsCore/Android.mk
@@ -15,6 +15,14 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := microg.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_SYSTEM)/
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := GmsCore
 LOCAL_SRC_FILES := GmsCore.apk
 LOCAL_MODULE_CLASS := APPS
@@ -22,7 +30,7 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_OVERRIDES_PACKAGES := com.qualcomm.location
-LOCAL_REQUIRED_MODULES := privapp-permissions-com.google.android.gms.xml sysconfig-com.google.android.gms.xml
+LOCAL_REQUIRED_MODULES := privapp-permissions-com.google.android.gms.xml sysconfig-com.google.android.gms.xml microg.xml
 # these lines will break builds before 19.1 so make them conditional
 ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
 LOCAL_USES_LIBRARIES := com.android.location.provider
