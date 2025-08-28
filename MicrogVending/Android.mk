ca4ee91ac0ff470c2136f6bc0150fb065e46de73
Sava Mikalacki <mikalackis@gmail.com>
2025-08-28 15:26:58 +0200

    Update apps and remove obsolete packages
    Change-Id: I2eba9a3cb4b8e991ed01f9ac6b904420c50c1fa9

---
+++ MicrogVending/Android.mk
@@ -0,0 +1,25 @@
+# This is the fake play store apk
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp-permissions-com.android.vending.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := MicrogVending
+LOCAL_SRC_FILES := com.android.vending-84022614.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_REQUIRED_MODULES := privapp-permissions-com.android.vending.xml
+LOCAL_CERTIFICATE := PRESIGNED
+# these lines will break builds before 19.1 so make them conditional
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
+endif
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_PREBUILT)
